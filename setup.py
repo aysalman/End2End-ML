5af1651d4682c3edfd8436ff87494da79fb82fcf
Ahmed Salman <69725263+aysalman@users.noreply.github.com>
2023-03-21 15:52:11 -0700

    Project Exception Logging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n"," ") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -25,3 +25,4 @@ setup(
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
+
